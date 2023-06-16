388aefc41f3dd0cb479ce87e78d3ade649e4a6f5
Ademola <sherggs@gmail.com>
2023-06-16 07:22:55 +0100

    Load by Username and find by email

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,11 +8,19 @@ import org.springframework.stereotype.Service;
 
 
 @Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
 
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+         return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
