1daebda98ca98f96baaed0f36961610d8de4a696
Ademola <sherggs@gmail.com>
2023-06-17 17:48:21 +0100

    Registration

---
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -0,0 +1,18 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "api/v1/registration")
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request) ;
+    }
+}
