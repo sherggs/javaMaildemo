e23dce1864567ddce72105e1f732fbbdc5c1678d
Ademola <sherggs@gmail.com>
2023-06-18 16:39:30 +0100

    Registration service

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,5 +1,10 @@
 package com.example.demo.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
+
+
 public class RegistrationService {
     public String register(RegistrationRequest request) {
 
