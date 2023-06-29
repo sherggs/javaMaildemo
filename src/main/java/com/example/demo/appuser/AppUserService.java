59ce3720605221f508af6a3a660e47193e87ff48
Ademola <sherggs@gmail.com>
2023-06-30 00:13:06 +0100

    Send Confirmation Token

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.demo.appuser;
 
+import com.example.demo.registration.token.ConfirmationToken;
+import com.example.demo.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 @Service
@@ -17,6 +22,7 @@ public class AppUserService implements UserDetailsService {
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -24,24 +30,38 @@ public class AppUserService implements UserDetailsService {
          return appUserRepository.findByEmail(email)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+         // throw exception if user not found
     }
 
     public String signUpUser(AppUser appUser) {
         boolean userExists = appUserRepository
-                .findByEmail(appUser.getEmail())
+                .findByEmail(appUser.getEmail()) // check if user exists
                 .isPresent();
         if(userExists) {
-            throw new IllegalStateException("email already taken");
+            throw new IllegalStateException("email already taken"); // throw exception if user already exists
         }
 
         String encodedPassword =  bCryptPasswordEncoder
-                .encode(appUser.getPassword());
+                .encode(appUser.getPassword()); // encode password
 
-        appUser.setPassword(encodedPassword);
+        appUser.setPassword(encodedPassword); // set password to encoded password
 
-        appUserRepository.save(appUser);
+        appUserRepository.save(appUser); // save user to db
+
         //TODO: Send confirmation token
-        return "it works ";
+        String token = UUID.randomUUID().toString(); // generate random token
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(), // token created at current time
+                LocalDateTime.now().plusMinutes(15), // token expires in 15 minutes
+                appUser
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken); // save token to db
+
+        // TODO: Send email
+
+        return token;
 
     }
 }
