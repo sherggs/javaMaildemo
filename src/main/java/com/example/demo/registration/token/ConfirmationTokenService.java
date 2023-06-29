cfcfe3824036c487463b505009e176da29c622ea
Ademola <sherggs@gmail.com>
2023-06-30 00:15:35 +0100

    Token Saved

--- src/main/java/com/example/demo/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationTokenService.java
@@ -1,7 +1,15 @@
 package com.example.demo.registration.token;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
 }
