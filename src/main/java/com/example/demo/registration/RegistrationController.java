468ee9ac7b74deecd34170f6b2587107e128dc82
Ademola <sherggs@gmail.com>
2023-07-02 23:58:17 +0100

    comment Registration Controller

--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = "api/v1/registration")
+@RequestMapping(path = "api/v1/registration") // http://localhost:8080/api/v1/registration
 public class RegistrationController {
 
     private final RegistrationService registrationService;
 
     public String register
             (@RequestBody RegistrationRequest request) {
-        return registrationService.register(request);
+        return registrationService.register(request); // this is the method we created in RegistrationService.java
     }
 }
