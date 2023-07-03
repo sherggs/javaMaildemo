7c3ba6a6eb337aac1be744e07bec0e37a3d6b888
Ademola <sherggs@gmail.com>
2023-07-03 23:52:16 +0100

    Email Sender

--- src/main/java/com/example/demo/email/EmailService.java
+++ src/main/java/com/example/demo/email/EmailService.java
@@ -1,9 +1,12 @@
 package com.example.demo.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +20,19 @@ public class EmailService implements EmailSender {
 
     @Override
     public void send(String to, String email) {
+        try{
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("sherggs@gmail.com");
+
+        } catch (MessagingException e) {
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
 
     }
 }
