e4b64655dca4bde66a869905aa3b4eb55076dad0
Ademola <sherggs@gmail.com>
2023-07-03 23:42:15 +0100

    mail logger

---
+++ src/main/java/com/example/demo/email/EmailService.java
@@ -0,0 +1,22 @@
+package com.example.demo.email;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailService.class);
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void send(String to, String email) {
+
+    }
+}
